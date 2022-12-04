2bed2a37c2aecb3719136f409e4e7e7a373c8616
LeMoonStar <webmaster@unitcore.de>
2022-12-04 14:24:31 +0100

    Day 4

--- src/days/d04.rs
+++ src/days/d04.rs
@@ -2,14 +2,37 @@ use super::{Answer, Day, DayImpl};
 
 const CURRENT_DAY: u8 = 4;
 
-type Data = Vec<u64>;
+#[derive(Debug, Clone)]
+pub struct Range(u64, u64);
+
+impl Range {
+    fn contains(&self, other: &Self) -> bool {
+        self.0 <= other.0 && other.1 <= self.1
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        self.0 <= other.1 && other.0 <= self.1
+    }
+}
+
+impl From<&str> for Range {
+    fn from(v: &str) -> Self {
+        let mut s = v.split("-");
+        Self(
+            s.next().unwrap().parse().unwrap(),
+            s.next().unwrap().parse().unwrap(),
+        )
+    }
+}
+
+type Data = Vec<Vec<Range>>;
 impl DayImpl<Data> for Day<CURRENT_DAY> {
     fn init_test() -> (Self, Data) {
         Self::init(include_str!("test_inputs/test04.txt"))
     }
 
     fn expected_results() -> (Answer, Answer) {
-        (Answer::Number(0), Answer::Number(0))
+        (Answer::Number(2), Answer::Number(4))
     }
 
     fn init(input: &str) -> (Self, Data) {
@@ -17,16 +40,28 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
             Self {},
             input
                 .lines()
-                .map(|v| v.parse::<u64>().expect("error while parsing input."))
+                .map(|v| v.split(",").map(|v| Range::from(v)).collect())
                 .collect(),
         )
     }
 
     fn one(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.len() as u64)
+        let mut c = 0;
+        for d in data {
+            if d[0].contains(&d[1]) || d[1].contains(&d[0]) {
+                c += 1;
+            }
+        }
+        Answer::Number(c)
     }
 
     fn two(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.len() as u64)
+        let mut c = 0;
+        for d in data {
+            if d[0].overlaps(&d[1]) {
+                c += 1;
+            }
+        }
+        Answer::Number(c)
     }
 }
